21a867578044248e01b4bf5881b80162e924221f
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2023-01-20 11:52:53 +0100

    Oppstart webshop

--- shopscript.js
+++ shopscript.js
@@ -8,6 +8,19 @@ function addToCart(title, price) {
     renderCart()
     //Så må vi oppdatere label med antall produkter
     document.querySelector("#cart .label").innerHTML = cart.length
+
+    //Flere måter å gjøre boksen synlig ved produkt innlegging
+    /*    if(document.getElementById("cartview").classList.contains("hidden")) {
+        toggleCart()
+    }*/
+
+    document.getElementById("cartview").classList.remove("hidden")
+}
+
+function calculatePrice() {
+    let totalPrice = 0
+    cart.map(prod => totalPrice += prod.productPrice)
+    document.getElementById("total").innerHTML = totalPrice
 }
 
 function renderCart() {
@@ -18,10 +31,16 @@ function renderCart() {
     <span class="title">${prod.productTitle}</span>
     <span class="price">${prod.productPrice}</span>
     <span class="quantity">${prod.productQuantity}</span>
-    <span class="delete">X</span>
+    <span class="delete" onClick="deleteProduct()">X</span>
 </li>`)
     //Bruke en selector for å finne riktig <ul>, og skrive inn listHTML:
     document.querySelector("#cartview ul").innerHTML = listHTML
+    //Oppdater total pris:
+    calculatePrice()
+}
+
+function deleteProduct(index) {
+    cart.splice(index, 1)
 }
 
 function toggleCart() {
